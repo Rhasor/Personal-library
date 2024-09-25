e9bfc2974ce41992605bcca777996527e4623e2a
Benerick <benerick.rivera.q@gmail.com>
2024-09-25 17:40:28 -0500

    Add Js toggleReadStatus fix

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
-function Book(title, author, pages, read) {
+const myLibrary = [];
+function Book(title, author, pages, comments, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.comments = comments;
+    this.read = read;  
 }
-
-const myLibrary = [];
-
 const addBookDialog = document.getElementById("add-book-dialog");
 const openAddDialogButton = document.getElementById("open-add-dialog-button");
 const closeDialogButton = document.getElementById("close-dialog-button");
 const form = document.getElementById("book-form");
 const addBookButton = document.getElementById("add-book-button");
-// site where appear cards
 const bookGrid = document.getElementById("book-grid");
 
-// open Add book Dialog
-openAddDialogButton.addEventListener('click', openAddBookDialog);
-function openAddBookDialog() {
-    addBookDialog.showModal();
-}
-// close Add book Dialog
-closeDialogButton.addEventListener('click', closeAddBookDialog);
-function closeAddBookDialog(event) {
-    event.preventDefault();
-    addBookDialog.close();
-    form.reset();
-}
 addBookButton.addEventListener('click', handleAddBook);
+
 function handleAddBook(event) {
     event.preventDefault();
     if(form.checkValidity()) {
         const book = new Book(
             form.elements["book-title"].value,
-            form.elements["book-author"].value,
+            form.elements["book-author"].value, 
             form.elements["book-pages"].value,
+            form.elements["book-comments"].value, 
             form.elements["book-read"].checked
         );
         addBookDialog.close();
@@ -44,7 +32,8 @@ function handleAddBook(event) {
     else {
         form.reportValidity();
     }
-} 
+}
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
     const newBookCard = createBookCard(book);
@@ -52,73 +41,65 @@ function addBookToLibrary(book) {
 }
 
 function createBookCard(book) {
-    const bookCard = document.createElement("div")
+    const bookCard = document.createElement("div");
     bookCard.className = "book-card";
-    bookCard.setAttribute("position", String(myLibrary.length - 1));
+    bookCard.setAttribute("position", myLibrary.length - 1);
+    const createElement = (type, className, text = "") => {
+        const element = document.createElement(type);
+        element.className = className;
+        element.innerText = text;
+        return element;
+    };
+    const titleDisplay = createElement("div", "title", book.title);
+    const authorDisplay = createElement("div", "author", book.author);
+    const pagesDisplay = createElement("div", "pages", `${book.pages} Pages`);
+    const commentsDisplay = createElement("div", "comments", `Comments: ${book.comments}`);
+    const readButton = createElement("button", "read-btn", book.read ? "Read" : "Unread");
+    const removeButton = createElement("i", "delete", "X");
 
-    const titleDisplay = document.createElement("div");
-    const authorDisplay = document.createElement("div");
-    const pagesDisplay = document.createElement("div");
-    const readButton = document.createElement("button");
-    const removeButton = document.createElement("i");
+    readButton.setAttribute("read", book.read ? "true" : "false");
 
-    titleDisplay.className = "title";
-    authorDisplay.className = "author";
-    pagesDisplay.className = "pages";
-    removeButton.className = "material-icons";
-
-    titleDisplay.innerText = book.title;
-    authorDisplay.innerText = book.author;
-    pagesDisplay.innerText = book.pages + " Pages";
-    removeButton.innerText = "delete";
-
-    if (book.read) {
-        readButton.setAttribute("read", "true")
-        readButton.innerText = "Read";
-    }
-    else {
-        readButton.setAttribute("read", "false")
-        readButton.innerText = "Unread";
-    }
     readButton.addEventListener('click', () => {
-        toggleReadStatus(book);
+        toggleReadStatus(myLibrary.indexOf(book));
     });
     removeButton.addEventListener('click', () => {
         deleteBook(book);
     });
 
-    titleDisplay.style.overflowWrap = "break-word";
-    titleDisplay.style.textAlign = "center";
-    authorDisplay.style.overflowWrap = "break-word";
-    authorDisplay.style.textAlign = "center";
-
-    bookCard.append(titleDisplay, authorDisplay, pagesDisplay, readButton, removeButton);
+    bookCard.append(removeButton, titleDisplay, authorDisplay, pagesDisplay, commentsDisplay, readButton);
     return bookCard;
 }
 
 function deleteBook(bookToDelete) {
     const index = myLibrary.indexOf(bookToDelete);
     const bookList = document.getElementsByClassName("book-card");
-    bookList[index].parentNode.removeChild(bookList[index]);
-    myLibrary.splice(index, 1);
-    for (let i = index; i < myLibrary.length; i++) {
-        bookList[i].setAttribute("position", String(Number(bookList[i].getAttribute("position"))- 1));
+    if (index > -1) {
+        bookList[index].remove(); 
+        myLibrary.splice(index, 1);
+        for (let i = index; i < bookList.length; i++) {
+            bookList[i].setAttribute("position", i);
+        }
     }
 }
 
-function toggleReadStatus(bookToToggle) {
-    bookToToggle.read = !bookToToggle.read;
-    
-    const index = myLibrary.indexOf(myLibrary.find(({title}) => title === bookToToggle.title));
-    const readButton = document.querySelector('div[position="${index}"] button');
-
-    if (bookToToggle.read) {
-        readButton.setAttribute("read","true");
+function toggleReadStatus(index) {
+    myLibrary[index].read = !myLibrary[index].read;
+    const readButton = document.querySelector(`div[position="${index}"] button`);
+    if (myLibrary[index].read) {              
+        readButton.setAttribute("read", "true");
         readButton.innerText = "Read";
-    }
-    else {
-        readButton.setAttribute("read","false");
+    } else {
+        readButton.setAttribute("read", "false");
         readButton.innerText = "Unread";
     }
 }
 
+openAddDialogButton.addEventListener('click', () => addBookDialog.showModal());
+closeDialogButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    addBookDialog.close();
+    form.reset();
+});
+
+addBookToLibrary(new Book("The Great Adventure", "John Doe", 350, "Great literary work to read in a weekend.", true));
+addBookToLibrary(new Book("Mystery of the Night", "Jane Smith", 220, "Interesting categorical outcome", false));
